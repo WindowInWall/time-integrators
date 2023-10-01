770246fe333fb9380e5e91b66616851f2b294a7b
Vivek Sarkar <vivek.sarkar@gmail.com>
2023-10-01 11:14:08 -0700

    added drag

--- el_ball_game.py
+++ el_ball_game.py
@@ -1,5 +1,6 @@
 # Example file showing a circle moving on screen
 import pygame
+import math
 
 # pygame setup
 pygame.init()
@@ -12,12 +13,16 @@ g = [0.0, 9.81]
 
 # Initial position, velocity, and acceleration vectors.
 init_pos = [0, 4]
-init_vel = [300, 100]
+init_vel = [100, 100]
 init_acc = [0, 0]
+
+# in milliCoulombs
 charge_mouse = 0.1
 charge_ball = 0.1
 
 mass = 30
+radius = 40
+player_radius = 40
 
 def mul_s2v(scalar, vec):
     return [scalar * e for e in vec]
@@ -45,7 +50,7 @@ def get_bounce(pos, m):
     return vec
 
 def get_el_force_mag(mouse_pos, pos):
-    return 9.0 * 10**9 * charge_ball * charge_mouse / ((mouse_pos[0] - pos[0])**2 + (mouse_pos[1] - pos[1])**2)
+    return 9.0 * 10.0 ** 9.0 * charge_ball * charge_mouse / ((mouse_pos[0] - pos[0])**2 + (mouse_pos[1] - pos[1])**2)
 
 def get_el_force(mouse_pos, pos):
     mag = get_el_force_mag(mouse_pos, pos)
@@ -55,17 +60,26 @@ def get_el_force(mouse_pos, pos):
     vec_norm = mul_s2v(1.0 / dist, vecvec)
     return mul_s2v(mag, vec_norm)
     
+def get_drag(vel):
+    op_vel = mul_s2v(-1, vel)
+    vel_mag = (op_vel[0] * op_vel[0] + op_vel[1] * op_vel[1]) ** (1 / 2)
+    norm_op_vel = mul_s2v(1 / vel_mag, op_vel)
 
-def get_force(pos, m):
+    # honey
+    dyn_viscosity = 0.05
+    drag_mag = 6 * math.pi * dyn_viscosity * radius * vel_mag
+    return mul_s2v(drag_mag, norm_op_vel)
+
+def get_force(pos, vel, m):
     gravity = mul_s2v(m, g)
     bounce = get_bounce(pos, m)
     el = get_el_force(list(pygame.mouse.get_pos()), pos)
-    return add_vecs(add_vecs(gravity, bounce), el)
-    #return add_vecs(gravity, bounce)
+    drag = get_drag(vel)
+    return add_vecs(add_vecs(add_vecs(gravity, bounce), el), drag)
+    #return add_vecs(add_vecs(gravity, bounce), el)
 
-
-def get_acc(pos, m):
-    return mul_s2v(1/m, get_force(pos, m))
+def get_acc(pos, vel, m):
+    return mul_s2v(1/m, get_force(pos, vel, m))
 
 def get_pos():
     """A generator yielding the ball's position at time t."""
@@ -75,7 +89,7 @@ def get_pos():
     while True:
         half_new_vel = add_vecs(vel, mul_s2v(0.5 * dt, acc))
         new_pos = add_vecs(pos, mul_s2v(dt, half_new_vel))
-        new_acc = get_acc(new_pos, mass)
+        new_acc = get_acc(new_pos, half_new_vel, mass)
         new_vel = add_vecs(half_new_vel, mul_s2v(0.5 * dt, new_acc))
 
         pos = new_pos
@@ -98,27 +112,11 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
 
-    pygame.draw.circle(screen, "red", player_pos, 40)
-    pygame.draw.circle(screen, "blue", particle_pos, 40)
+    pygame.draw.circle(screen, "red", player_pos, player_radius)
+    pygame.draw.circle(screen, "blue", particle_pos, radius)
 
-    (x, y) = pygame.mouse.get_pos()
-    player_pos.x = x
-    player_pos.y = y
-    
-    (pp_x, pp_y) = next(gen)
-    particle_pos.x = pp_x
-    particle_pos.y = pp_y
-    """
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
-    """
+    player_pos.x, player_pos.y = pygame.mouse.get_pos()
+    particle_pos.x, particle_pos.y = next(gen)
 
     # flip() the display to put your work on screen
     pygame.display.flip()
