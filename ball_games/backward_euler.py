c5bf81d24f98072a440ca1f64660bdecefc74071
masan <mnakura@uw.edu>
2023-10-13 14:38:40 -0700

    fixed backwards euler

--- ball_games/backward_euler.py
+++ ball_games/backward_euler.py
@@ -1,20 +1,26 @@
 # Example file showing a circle moving on screen
 import pygame
 import math
+import numpy as np
 
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
-dt = 0
-
+dt = clock.tick(60) / 1000
 g = [0, 9.81]
 
 # Initial position, velocity, and acceleration vectors.
 init_pos = [screen.get_width() / 2, 0]
 init_vel = [0, 0]
 init_acc = [0, 0]
+d = 0
+w = 10
+init_x = np.array([500, 0])
+A = np.array([[0, 1],
+              [-w**2, -2 * d * w]])
+print(A.shape)
 
 # in milliCoulombs
 charge_mouse = 0.1
@@ -26,85 +32,13 @@ d = 2
 radius = 40
 player_radius = 40
 
-def mul_s2v(scalar, vec):
-    return [scalar * e for e in vec]
-
-def add_vecs(vec1, vec2):
-    return [x + y for x, y in zip(vec1, vec2)]
-
-def get_bounce(pos, m):
-    acc = 10000
-    
-    XMAX, YMAX = pygame.display.get_surface().get_size()
-    
-    vec = [0, 0]
-
-    if pos[0] < 0:
-        vec = add_vecs(vec, [m * acc, 0.0])
-    elif pos[0] > XMAX:
-        vec = add_vecs(vec, [-m * acc, 0.0])
-
-    if pos[1] < 0:
-        vec = add_vecs(vec, [0.0, m * acc])
-    elif pos[1] > YMAX:
-        vec = add_vecs(vec, [0.0, -m * acc])
-
-    return vec
-
-def get_el_force_mag(mouse_pos, pos):
-    return 9.0 * 10.0 ** 9.0 * charge_ball * charge_mouse / ((mouse_pos[0] - pos[0])**2 + (mouse_pos[1] - pos[1])**2)
-
-def get_el_force(mouse_pos, pos):
-    mag = get_el_force_mag(mouse_pos, pos)
-    vecvec = add_vecs(pos, mul_s2v(-1, mouse_pos))
-    dist = ((mouse_pos[0] - pos[0])**2 + (mouse_pos[1] - pos[1])**2) ** (1/2)
-    
-    vec_norm = mul_s2v(1.0 / dist, vecvec)
-    return mul_s2v(mag, vec_norm)
-    
-def get_drag(vel):
-    op_vel = mul_s2v(-1, vel)
-    vel_mag = (op_vel[0] * op_vel[0] + op_vel[1] * op_vel[1]) ** (1 / 2)
-    norm_op_vel = mul_s2v(1 / vel_mag, op_vel)
-
-    # honey
-    dyn_viscosity = 0.05
-    drag_mag = 6 * math.pi * dyn_viscosity * radius * vel_mag
-    return mul_s2v(drag_mag, norm_op_vel)
-
-def get_force(pos, vel, m):
-    gravity = mul_s2v(m, g)
-    bounce = get_bounce(pos, m)
-    el = get_el_force(list(pygame.mouse.get_pos()), pos)
-    drag = get_drag(vel)
-    return add_vecs(add_vecs(add_vecs(gravity, bounce), el), drag)
-    #return add_vecs(add_vecs(gravity, bounce), el)
-
-def get_acc(pos, vel, m):
-    return mul_s2v(1/m, get_force(pos, vel, m))
-
-def getNewVel(pos,vel, m):
-    vec = mul_s2v(1 - ((k/m) * (dt**2)) + ((d/m) * dt), add_vecs(add_vecs(vel, mul_s2v(dt, g)), mul_s2v(k/m * dt, pos)))
-    vec[0] = 0
-    return vec
-
 def get_pos():
     """A generator yielding the ball's position at time t."""
-    pos = init_pos
-    vel = init_vel
-    #acc = init_acc
+    print('should be zero:')
+    x = init_x
     while True:
-        print(vel[1])
-        new_vel = add_vecs(vel, getNewVel(pos, vel, mass))
-        print(vel[0])
-        new_pos = add_vecs(pos, mul_s2v(dt, vel))
-        #new_acc = get_acc(new_pos, new_vel, mass)
-
-        pos = new_pos
-        vel = new_vel
-        #acc = new_acc
-
-        yield (pos[0], pos[1])
+        x = np.linalg.pinv(np.eye(2) - A.dot(dt)).dot(x)
+        yield x[0]
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 particle_pos = pygame.Vector2(screen.get_width() / 4, screen.get_height() / 2)
@@ -125,7 +59,8 @@ while running:
     pygame.draw.circle(screen, "blue", particle_pos, radius)
 
     player_pos.x, player_pos.y = pygame.mouse.get_pos()
-    particle_pos.x, particle_pos.y = next(gen)
+    particle_pos.x = next(gen) + screen.get_width() / 2
+    particle_pos.y = screen.get_height() / 2
 
     # flip() the display to put your work on screen
     pygame.display.flip()
